e00e58d5b20786ac277de7e0b0ea82ab397cef76
Pedro Vinicius <139568403+PedrooVi@users.noreply.github.com>
2023-12-20 11:25:25 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Calculadora de IMC (Indice de massa corporal) simples, utilizando HTML5, CSS, e JavaScript
+Alunos: Pedro V Correa, Marcus V Mendes
--- src/script.js
+++ src/script.js
@@ -15,20 +15,20 @@ function calcular(){
         alert("Por favor, informe um peso e uma altura para começar")
     }else if(imc <18.5 ){
         resultado.innerHTML = `Seu IMC é de ${imc.toFixed(2)} voce está abaixo do peso`
-        areaTxt.style.backgroundColor = 'red'
+        areaTxt.style.backgroundColor = 'crimson'
     } else if(imc >= 18.5 &&imc <25 ){
         resultado.innerHTML = `Seu IMC é de ${imc.toFixed(2)} você está no peso ideal`
-        areaTxt.style.backgroundColor ='green'
+        areaTxt.style.backgroundColor ='forestgreen'
     }
     else if (imc >= 25 && imc <29.9){
         resultado.innerHTML= `Seu IMC é de ${imc.toFixed(2)} você está levemente acima do peso`
-        areaTxt.style.backgroundColor='yellow'
+        areaTxt.style.backgroundColor='gold'
     }else if(imc >= 30 && imc < 34.9){
         resultado.innerHTML = `Seu IMC é de ${imc.toFixed(2)} voce está obeso`
-        areaTxt.style.backgroundColor = 'red'
+        areaTxt.style.backgroundColor = 'crimson'
     } else if (imc >= 35 && imc <39.9){
         resultado.innerHTML = `Seu IMC é de ${imc.toFixed(2)} você está com obesidade severa`
-        areaTxt.style.backgroundColor='red'
+        areaTxt.style.backgroundColor='crimson'
     }else{
         resultado.inert =  resultado.innerHTML = `Seu IMC é de ${imc.toFixed(2)} você está com obesidade morbida`
         areaTxt.style.backgroundColor='red'
